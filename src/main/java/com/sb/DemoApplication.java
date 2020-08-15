963b593951d6fac207ab3ef0f92526a78d0a0adb
sriranga97 <prahladha97@gmail.com>
2020-08-16 01:36:33 +0530

    stashpractice

--- src/main/java/com/sb/DemoApplication.java
+++ src/main/java/com/sb/DemoApplication.java
@@ -8,7 +8,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+<<<<<<< Updated upstream
 		System.out.println("this is ranganath chowdary ,kamlini mukarji");
+=======
+		System.out.println("this is ranganath  ");
+>>>>>>> Stashed changes
 	}
 
 }
